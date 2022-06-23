eb1b3d631b837f4bfb0caef8ed42bc63de85fb04
vanikulkarni202 <vanikulkarni202@gmail.com>
2022-06-23 11:27:38 +0530

    AI_project

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template, jsonify, request
 import processor
+
+
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
+
+# The locals() method updates and returns a dictionary of the current local symbol table.
+#  A symbol table is a data structure maintained by a compiler which contains all 
+# necessary information about the program.
+#  These include variable names, methods, classes, etc. There are mainly two kinds of symbol table.
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', **locals())
+    # control hits to index page initial
+
+
+
 @app.route('/chatbot', methods=["GET", "POST"])
 def chatbotResponse():
+
     if request.method == 'POST':
         the_question = request.form['question']
+
         response = processor.chatbot_response(the_question)
+
     return jsonify({"response": response })
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8000', debug=True)
+    app.run(host='0.0.0.0', port='8001', debug=True)
--- processor.py
+++ processor.py
@@ -3,6 +3,7 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
+
 from keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
@@ -10,11 +11,15 @@ import random
 intents = json.loads(open('job_intents.json', encoding='utf-8').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
+
+
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
+
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
@@ -28,6 +33,7 @@ def bow(sentence, words, show_details=True):
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
+
 def predict_class(sentence, model):
     # filter out predictions below a threshold
     p = bow(sentence, words, show_details=False)
@@ -40,6 +46,7 @@ def predict_class(sentence, model):
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
+
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
@@ -50,6 +57,7 @@ def getResponse(ints, intents_json):
         else:
             result = "You must ask the right questions"
     return result
+
 def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
